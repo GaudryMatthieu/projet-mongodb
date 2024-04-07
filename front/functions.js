2334bdd55d6ef4ac7390f25d811f89049984535b
matthieu <matthieu.gaudry78@gmail.com>
2024-04-08 00:03:56 +0200

    color OK

--- front/functions.js
+++ front/functions.js
@@ -53,7 +53,17 @@ function readTaskByDay(day) {
             data.forEach(task => {
                 const button = document.createElement('button');
                 button.textContent = task.title;
-                button.className = "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800";
+                if (task.color == "green") {
+                    button.className = "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800";
+                } else if (task.color == "yellow") {
+                    button.className = "focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900";
+                } else if (task.color == "purple") {
+                    button.className = "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900";
+                } else if (task.color == "red"){
+                    button.className = "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900";
+                } else {
+                    button.className = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
+                }
                 button.addEventListener('click', (event) => {
                     event.stopPropagation();
                     displayContent(task.title, task.description, task._id, task.color, task.day);
